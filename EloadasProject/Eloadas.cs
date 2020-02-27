fa3debbe91929c1609333d9f2e6aa430e39b12c1
László Pálfalvi <palflaszlo20@outlook.hu>
2020-02-27 23:20:10 +0100

    kesz

---
+++ EloadasProject/Eloadas.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EloadasProject
+{
+    public class Eloadas
+    {
+        public bool[,] foglalasok;
+        public int sorokSzama;
+        public int helyekSzama;
+        public Eloadas(int sorokSzama, int helyekSzama)
+        {
+            if(sorokSzama < 0 || helyekSzama < 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                foglalasok = new bool[sorokSzama, helyekSzama];
+            }
+        }
+
+        public bool[,] Foglalasok { get => foglalasok; set => foglalasok = value; }
+
+        public bool Lefoglal()
+        {
+            for (int i = 0; i < sorokSzama; i++)
+            {
+                for (int j = 0; j < helyekSzama; j++)
+                {
+                    if (!foglalasok[i, j])
+                    {
+                        foglalasok[i, j] = true;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public int szabadHelyek()
+        {
+            int count = 0;
+            for (int i = 0; i < Foglalasok.GetLength(0); i++)
+            {
+                for (int j = 0; j < Foglalasok.GetLength(1); j++)
+                {
+                    if (!foglalasok[i, j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public bool Teli()
+        {
+            int count = szabadHelyek();
+            
+            if(count == 0)
+            {
+                Console.WriteLine("Az előadás tele van.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Még van szabad hely.");
+                return false;
+            }
+        }
+
+        public bool Foglalt(int sorSzam, int helySzam)
+        {
+            if (sorokSzama < 0 || helyekSzama < 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                for (int i = 0; i < sorokSzama; i++)
+                {
+                    for (int j = 0; j < helyekSzama; j++)
+                    {
+                        if (i == sorSzam && j == helySzam)
+                        {
+                            if (!foglalasok[i, j])
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ EloadasProject/Tests/EloadasTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EloadasProject.Tests
+{
+    [TestFixture]
+    class EloadasTests
+    {
+        Eloadas eload;
+
+        [TestCase]
+        public void negativErtek()
+        {
+            Assert.Pass("jeee", eload = new Eloadas(-53, -23));
+        }
+
+        [TestCase]
+        public void szabadUles()
+        {
+            eload = new Eloadas(323, 321);
+            Assert.AreEqual(false, eload.Foglalt(43,123), "a ules szabad");
+        }
+        [TestCase]
+        public void szabadUles2()
+        {
+            eload = new Eloadas(323, 321);
+            Assert.AreEqual(false, eload.Foglalt(-302,-21), "Hibás paraméter!");
+        }
+        [TestCase]
+        public void szabadUles3()
+        {
+            eload = new Eloadas(323, 321);
+            eload.Lefoglal();
+            Assert.AreEqual(true, eload.Foglalt(0, 0), "Sikeres foglalás!");
+        }
+        [TestCase]
+        public void TelivanE()
+        {
+            eload = new Eloadas(323, 321);
+            Assert.IsFalse(eload.Teli(), "A terem nincs tele");
+        }
+        [TestCase]
+        public void TelevanE()
+        {
+            eload = new Eloadas(1, 2);
+            eload.Lefoglal();
+            eload.Lefoglal();
+
+            Assert.IsFalse(eload.Teli(), "Az a terem tele van");
+        }        
+
+        [TestCase]
+        public void dobjonArgumentet()
+        {
+            Assert.Throws<ArgumentException>(szabadUles2);
+            Assert.Throws<ArgumentException>(() => {
+                var eloadas2 = new Eloadas(-32, -312);
+            });
+        }
+        [TestCase]
+        public void dobjonArgumentet2()
+        {
+            Assert.Throws<ArgumentException>(szabadUles3);
+            Assert.Throws<ArgumentException>(() => {
+                eload.Foglalt(-21, 302);
+            });
+        }
+        [TestCase]
+        public void dobjonArgumentet3()
+        {
+            Assert.Throws<ArgumentException>(TelivanE);
+            Assert.Throws<ArgumentException>(() => {
+                eload.Foglalt(100, 50);
+            });
+        }
+        [TestCase]
+        public void dobjonArgumentet4()
+        {
+            Assert.Throws<ArgumentException>(TelevanE);
+            Assert.Throws<ArgumentException>(() => {
+                eload.Foglalt(-31, 20);
+            });
+        }
+    }
+}
